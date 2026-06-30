7bb02ea8878ad2ac4d007023c18326df3f6bb156
100kgtrotila <hirodevi192@gmail.com>
2026-06-30 13:49:46 +0300

    fix: remove unused serverExternalPackages from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  serverExternalPackages: ["firebase-admin", "jose", "jwks-rsa"],
 };
 
 export default nextConfig;
